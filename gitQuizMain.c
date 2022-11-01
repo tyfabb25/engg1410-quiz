be8c432422045f084da7ce65d6330d4cc61a65d0
Tyler <tfabbric@uoguelph.ca>
2022-11-01 13:06:56 -0400

    Updated gitMainQuiz to have else function

--- gitQuizMain.c
+++ gitQuizMain.c
@@ -10,6 +10,10 @@ int main(void)
     if(val == 'y'){
         printf("You know how to use Git!");
     }
+    else{
+        printf("You don't know how to use git :( ")
+    }
+
 
     printf("\n");
 
